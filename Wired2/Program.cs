9238b8b8070cf614671b3f544571937d59ad341e
Nicholas Scott <nicholasscott@outlook.com>
2021-06-17 18:53:14 +0100

    here it is

--- Wired2/Program.cs
+++ Wired2/Program.cs
@@ -9,6 +9,7 @@ namespace Wired2
         /// </summary>
         /// <param name="args"></param>
         /// julia
+        /// yet anothyer change to conflict
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
