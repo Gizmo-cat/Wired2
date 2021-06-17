121032c90abfaa5b630d385ace96ba59cee19b86
Nicholas Scott <nicholasscott@outlook.com>
2021-06-17 16:58:09 +0100

    added julia

--- Wired2/Program.cs
+++ Wired2/Program.cs
@@ -8,6 +8,7 @@ namespace Wired2
         /// ///
         /// </summary>
         /// <param name="args"></param>
+        /// julia
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
